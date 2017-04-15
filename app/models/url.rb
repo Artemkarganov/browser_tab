e41d4211dbc9a0c4a35cbb3560da6b82cb884d07
Artem_Karganov <artem.karganov@gmail.com>
2017-04-15 08:36:27 +0300

    Implemented search by url

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,5 +2,13 @@ class Url < ApplicationRecord
   validates :url, presence: true, :format => URI::regexp(%w(http https))
   validates :name, presence: true
 
+  def self.search(search)
+    if search
+     where('url LIKE ?', "%#{search}%")
+    else
+     all
+    end
+  end
+
   #belongs_to :user
 end
