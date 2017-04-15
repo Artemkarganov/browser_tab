e8d1ced472c79636b6de2448d0155414a8303c90
Artem_Karganov <artem.karganov@gmail.com>
2017-04-15 08:04:45 +0300

    Added login with Facebook only. Display the content only after loggin, added the urls creation process

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,42 @@
+class UrlsController < ApplicationController
+  before_action :authorize
+
+  def index
+    @urls = Url.all
+  end
+
+  def show
+    unless @url = Url.find(params[:id])
+      render text: "Page not found", status: "404"
+    end
+  end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.new(url_params)
+    respond_to do |format|
+       if @url.save
+         format.html { redirect_to @url, notice: 'Url was successfully created.' }
+         format.json { render :show, status: :created, location: @url }
+       else
+         format.html { render :new }
+         format.json { render json: @url.errors, status: :unprocessable_entity }
+       end
+     end
+  end
+
+  def destroy
+    @url = Url.find(params[:id])
+    @url.destroy
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:url, :name)
+  end
+end
